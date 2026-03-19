87fa0a50a69782c3da949e8efaf636db415f2fb3
Vikashh08 <vikashroy2333@gmail.com>
2026-03-19 13:30:40 +0530

    Netlify.toml file added

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const allowedOrigins = [
   "http://localhost:5175",
   "https://spixouser.netlify.app",
   "https://spixoadmin.netlify.app",
-  "https://spixodeliverys.netlify.app"
+  "https://spixodeliveries.netlify.app"
 ];
 
 export const io = new Server(server, {
